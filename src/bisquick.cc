2330d74c697614bf77159f09422446c1eba47ab4
Luis Enrique Ramirez Chavez <sccluisx@gmail.com>
2016-08-22 02:38:28 +0200

    Autotools configuration

--- src/bisquick.cc
+++ src/bisquick.cc
@@ -4,8 +4,27 @@
 
 using namespace seqan;
 
-int int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   std::cout<<"Bisquick v.0.01"<<std::endl;
+  // Initialization
+  String<char> text = "This is an awesome tutorial to get to know SeqAn!";
+  String<char> pattern = "tutorial";
+  String<int> score;
+  resize(score, length(text) - length(pattern) + 1);
+  // Computation of the similarities
+  // Iteration over the text (outer loop)
+  for (unsigned i = 0; i < length(text) - length(pattern) + 1; ++i) {
+      int localScore = 0;
+      // Iteration over the pattern for character comparison
+      for (unsigned j = 0; j < length(pattern); ++j) {
+          if (text[i + j] == pattern[j])
+              ++localScore;
+      }
+      score[i] = localScore;
+  }
+  // Printing the result
+  for (unsigned i = 0; i < length(score); ++i)
+      std::cout << score[i] << " ";
+  std::cout << std::endl;
   return 0;
 }
